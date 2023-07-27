1c184dd669eb5ea3a824a7d4ec67466babdc76c2
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-07-27 18:46:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -103,33 +103,19 @@ clearCartButton.addEventListener("click", clearCart);
     { id: 1, name: "Product 1", price: 10 },
     { id: 5, name: "Product 5", price: 50 },
   ];
-() => {
-  // Click the "Add to Cart" button for the first product
-  cy.get("ul#product-list").children("li").first().children("button").click();
-
-  // Ensure the correct products are in the cart
-  const expectedProducts = [
-    { id: 1, name: "Product 1", price: 10 },
-    { id: 5, name: "Product 5", price: 50 },
-  ];
 
   // Check the cart in session storage
-  cy.window()
-    .its("sessionStorage")
-    .should((sessionStorage) => {
-      const cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+  cy.window().its("sessionStorage").should((sessionStorage) => {
+    const cart = JSON.parse(sessionStorage.getItem("cart")) || [];
 
-      // Custom assertion to check for the presence of expected products in the cart
-      expect(cart).to.have.length(expectedProducts.length);
+    // Custom assertion to check for the presence of expected products in the cart
+    expect(cart).to.have.length(expectedProducts.length);
 
-      // Sort both the cart and expectedProducts arrays by product id
-      const sortedCart = cart.slice().sort((a, b) => a.id - b.id);
-      const sortedExpectedProducts = expectedProducts.slice().sort((a, b) => a.id - b.id);
-
-      expect(sortedCart).to.deep.equal(sortedExpectedProducts);
+    expectedProducts.forEach((expectedProduct) => {
+      expect(cart).to.deep.include(expectedProduct);
     });
+  });
 }
-
 
 
 
